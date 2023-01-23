fef72016a04f9c1bbffc9b5c70625641d9fe52f9
fareed069417 <shoeb.ahmad@v-empower.com>
2023-01-23 22:08:39 +0530

    Bug fixes in  SDK 2.0.1

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,20 @@
-import { MeetHourExternalAPI } from './MeetHourExternalAPI';
+import { MeetHourExternalAPI } from "./MeetHourExternalAPI";
 declare global {
-    interface Window {
-        MeetHourExternalAPI: MeetHourExternalAPI;
-    }
+  interface Window {
+    MeetHourExternalAPI: MeetHourExternalAPI;
+  }
 }
 
-export { MeetHourExternalAPI } from './MeetHourExternalAPI';
-export { default as IMeetHourMeetingProps } from './IMeetHourMeetingProps';
-export { default as IMeetHourExternalApi } from './IMeetHourExternalApi';
-
+export { MeetHourExternalAPI } from "./MeetHourExternalAPI";
+export { default as IMeetHourMeetingProps } from "./IMeetHourMeetingProps";
+export { default as IMeetHourExternalApi } from "./IMeetHourExternalApi";
+export { default as AddContactType } from "./AddContactType";
+export { default as ContactsType } from "./ContactsType";
+export { default as EditContactType } from "./EditContactType";
+export { default as EditMeetingType } from "./EditMeetingType";
+export { default as GenerateJwtType } from "./GenerateJwtType";
+export { default as LoginType } from "./LoginType";
+export { default as RecordingsList } from "./RecordingsList";
+export { default as RefreshTokenType } from "./RefreshTokenType";
+export { default as ScheduleMeetingType } from "./ScheduleMeetingType";
+export { UserObjectType } from "./ScheduleMeetingType";
